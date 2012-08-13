bc8bd5c62b96963d25c1f497e92cba5e4cb55d65
Andrew Dobbs <andrew.dobbs@gmail.com>
2012-08-13 12:22:43 -0400

    Clean up unused code

--- js/app.js
+++ js/app.js
@@ -1,18 +1,8 @@
 function TodoCtrl($scope) {
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
+  $scope.todos = [];
  
   $scope.addTodo = function() {
     $scope.todos.push({text:$scope.todoText, done:false});
     $scope.todoText = '';
-  };
- 
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
   };
 }
